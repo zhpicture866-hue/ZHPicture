0a18522abf37b5b64bb3b6a6d74c179369d71878
zoelhapner <achmadzulkiflinurrochim@gmail.com>
2026-09-07 16:28:08 +0700

    update akuntansi lagi

--- app/Http/Controllers/AccountingAccountController.php
+++ app/Http/Controllers/AccountingAccountController.php
@@ -280,12 +280,14 @@ public function update(Request $request, AccountingAccount $account)
             'required',
             'string',
             'max:255',
-            Rule::unique('accounting_accounts', 'account_code')
+            Rule::unique(AccountingAccount::class, 'account_code')
                 ->ignore($account->id),
         ],
 
         'account_name' => 'required|string|max:255',
+
         'category' => 'required|string|max:255',
+
         'sub_category' => 'required|string|max:255',
 
         'initial_balance' => 'nullable|numeric',
@@ -295,7 +297,7 @@ public function update(Request $request, AccountingAccount $account)
         'parent_id' => [
             'nullable',
             'uuid',
-            Rule::exists('accounting_accounts', 'id')
+            Rule::exists(AccountingAccount::class, 'id')
                 ->where('license_id', $licenseId),
         ],
 
